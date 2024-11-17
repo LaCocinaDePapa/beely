41166f798017a31f355d4c7db1baa5b6297232e8
Juan Alberto <jarscode.dev@outlook.com>
2024-11-17 17:04:04 +0100

    some style changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -23,98 +23,97 @@ export const Login = () => {
   };
 
   return (
-    <section className="flex justify-center px-4 mt-28 min-h-min sm:px-6 lg:px-8 lg:mt-40">
-      <div className="w-full max-w-md">
-        <div>
-          <img src="/bee.svg" alt="logo" className="mx-auto size-12" />
-          <h2 className="mt-2 text-3xl font-extrabold text-center text-blue-600 dark:text-white">
-            Login into your account
-          </h2>
+    <section>
+      <div className="flex flex-col items-center justify-center px-6 mx-auto mt-8 lg:mt-24 md:mt-24">
+        <a
+          href="/"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+        >
+          <img className="mr-2 size-10" src="/bee.svg" alt="logo" />
+          Bee.ly
+        </a>
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Welcome back
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <label
+                  for="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="block w-full p-2 text-gray-900 border border-gray-300 rounded-md bg-gray-50 focus:ring-primary-600 focus:border-primary-600 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="block w-full p-2 text-gray-900 border border-gray-300 rounded-md bg-gray-50 focus:ring-primary-600 focus:border-primary-600 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      type="checkbox"
+                      className="w-4 border border-gray-300 rounded h-14 bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                      required
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label
+                      for="remember"
+                      className="text-gray-500 dark:text-gray-300"
+                    >
+                      Remember me
+                    </label>
+                  </div>
+                </div>
+                <a
+                  href="#"
+                  className="text-sm font-medium text-blue-500 hover:underline"
+                >
+                  Forgot password?
+                </a>
+              </div>
+
+              <Button type="submit" className="w-full py-2 text-[14.9px]">
+                Sign in to your account
+              </Button>
+
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Don’t have an account yet?{" "}
+                <Link
+                  to="/signup"
+                  className="font-medium text-blue-500 hover:underline"
+                >
+                  Sign up
+                </Link>
+              </p>
+            </form>
+          </div>
         </div>
-
-        <form onSubmit={handleSubmit} className="flex flex-col gap-8 mt-16">
-          <input type="hidden" name="remember" value="true" />
-          <div className="flex flex-col gap-4">
-            <div className="relative">
-              <label
-                htmlFor="email-address"
-                className="text-black dark:text-white"
-              >
-                Email address
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email-address"
-                autoComplete="email"
-                required
-                className="block w-full px-3 py-3 bg-transparent border rounded-md appearance-none border-neutral-300 placeholder:text-neutral-700 dark:placeholder:text-neutral-300 dark:border-neutral-800 sm:text-sm"
-                placeholder="Email address"
-              />
-            </div>
-
-            <div className="relative">
-              <label htmlFor="password" className="text-black dark:text-white">
-                Password
-              </label>
-              <input
-                type={showPassword ? "text" : "password"}
-                name="password"
-                id="password"
-                autoComplete="current-password"
-                required
-                className="block w-full px-3 py-3 bg-transparent border rounded-md appearance-none border-neutral-300 placeholder:text-neutral-700 dark:placeholder:text-neutral-300 dark:border-neutral-800 sm:text-sm"
-                placeholder="Password"
-              />
-
-              <EyeButton
-                showPassword={showPassword}
-                setShowPassword={setShowPassword}
-              />
-            </div>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                type="checkbox"
-                id="remember-me"
-                name="remember-me"
-                className="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500"
-              />
-              <label
-                htmlFor="remember-me"
-                className="block ml-2 text-sm text-gray-700 dark:text-white"
-              >
-                Remember me
-              </label>
-            </div>
-
-            <div className="text-sm">
-              <a
-                href="/#"
-                className="form-medium text-sky-600 hover:text-sky-700 dark:text-slate-300 dark:hover:text-slate-400"
-              >
-                Forgot your password ?
-              </a>
-            </div>
-          </div>
-
-          <div>
-            <Button type="submit" variant="secondary" size="lg">
-              Sign in
-            </Button>
-          </div>
-        </form>
-
-        <p className="mt-8">
-          <Link
-            className="text-sky-600 hover:text-sky-700 dark:text-slate-300 dark:hover:text-slate-400"
-            to="/signup"
-          >
-            Don't have an account ?
-          </Link>
-        </p>
       </div>
     </section>
   );
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -23,126 +23,113 @@ export const Register = () => {
   };
 
   return (
-    <section className="flex justify-center px-4 mt-28 min-h-min sm:px-6 lg:px-8 lg:mt-40">
-      <div className="w-full max-w-md space-y-8">
-        <div>
-          <h2 className="mt-2 text-3xl font-extrabold text-center text-blue-600 dark:text-white">
-            Get started at bee.ly
-          </h2>
+    <section>
+      <div className="flex flex-col items-center justify-center px-6 mx-auto mt-8 lg:mt-24 md:mt-24">
+        <a
+          href="/"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+        >
+          <img className="mr-2 size-10" src="/bee.svg" alt="logo" />
+          Bee.ly
+        </a>
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Create an account
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="block w-full p-2 text-gray-900 border border-gray-300 rounded-md bg-gray-50 focus:ring-primary-600 focus:border-primary-600 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="block w-full p-2 text-gray-900 border border-gray-300 rounded-md bg-gray-50 focus:ring-primary-600 focus:border-primary-600 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="confirmPassword"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Confirm password
+                </label>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  id="confirmPassword"
+                  placeholder="••••••••"
+                  className="block w-full p-2 text-gray-900 border border-gray-300 rounded-md bg-gray-50 focus:ring-primary-600 focus:border-primary-600 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="terms"
+                      aria-describedby="terms"
+                      type="checkbox"
+                      className="w-4 border border-gray-300 rounded h-14 bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                      required
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label
+                      htmlFor="terms"
+                      className="text-gray-500 dark:text-gray-300"
+                    >
+                      I agree to the{" "}
+                      <a
+                        href="/terms"
+                        className="font-medium text-blue-500 hover:underline"
+                      >
+                        Terms and Conditions
+                      </a>
+                    </label>
+                  </div>
+                </div>
+              </div>
+
+              <Button type="submit" className="w-full py-2 text-[14.9px]">
+                Create your account
+              </Button>
+
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Already have an account?{" "}
+                <Link
+                  href="/signin"
+                  className="font-medium text-blue-500 hover:underline"
+                >
+                  Sign in
+                </Link>
+              </p>
+            </form>
+          </div>
         </div>
-
-        <form onSubmit={handleSubmit} className="flex flex-col gap-8">
-          <div className="flex flex-col gap-4">
-            <div className="relative">
-              <label
-                htmlFor="complete-name"
-                className="text-black dark:text-white"
-              >
-                Complete name
-              </label>
-              <input
-                type="text"
-                name="name"
-                id="complete-name"
-                autoComplete="name"
-                required
-                className="relative block w-full px-3 py-3 border border-gray-700 rounded-md appearance-none focus:outline-none focus:ring-blue-400 focus:border-blue-400 focus:z-10 sm:text-sm"
-                placeholder="Complete name"
-              />
-            </div>
-
-            <div className="relative">
-              <label
-                htmlFor="email-address"
-                className="text-black dark:text-white"
-              >
-                Email address
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email-address"
-                autoComplete="email"
-                required
-                className="relative block w-full px-3 py-3 border border-gray-700 rounded-md appearance-none focus:outline-none focus:ring-blue-400 focus:border-blue-400 focus:z-10 sm:text-sm"
-                placeholder="Email address"
-              />
-            </div>
-
-            <div className="relative">
-              <label htmlFor="password" className="text-black dark:text-white">
-                Password
-              </label>
-              <input
-                type={showPassword ? "text" : "password"}
-                name="password"
-                id="password"
-                autoComplete="current-password"
-                required
-                className="relative block w-full px-3 py-3 border border-gray-700 rounded-md appearance-none focus:outline-none focus:ring-blue-400 focus:border-blue-400 focus:z-10 sm:text-sm"
-                placeholder="Password"
-              />
-
-              <EyeButton
-                showPassword={showPassword}
-                setShowPassword={setShowPassword}
-              />
-            </div>
-
-            <div className="relative">
-              <label
-                htmlFor="confirmPassword"
-                className="text-black dark:text-white"
-              >
-                Confirm password
-              </label>
-              <input
-                type={showPassword ? "text" : "password"}
-                name="confirmPassword"
-                id="confirmPassword"
-                autoComplete="current-password"
-                required
-                className="relative block w-full px-3 py-3 border border-gray-700 rounded-md appearance-none focus:outline-none focus:ring-blue-400 focus:border-blue-400 focus:z-10 sm:text-sm"
-                placeholder="Confirm pasword"
-              />
-
-              <EyeButton
-                showPassword={showPassword}
-                setShowPassword={setShowPassword}
-              />
-            </div>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                type="checkbox"
-                id="remember-me"
-                name="remember-me"
-                className="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-blue-400"
-              />
-              <label
-                htmlFor="remember-me"
-                className="block ml-2 text-sm text-gray-700 dark:text-white"
-              >
-                Remember me
-              </label>
-            </div>
-
-            <div className="text-sm">
-              <Link
-                to="/signin"
-                className="form-medium text-sky-600 hover:text-sky-700 dark:text-slate-300 dark:hover:text-slate-40"
-              >
-                Already have an account ?
-              </Link>
-            </div>
-          </div>
-
-          <div>
-            <Button variant="secondary">Signup</Button>
-          </div>
-        </form>
       </div>
     </section>
   );
