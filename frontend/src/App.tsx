07dd3390b3bd16d392214d3972ed26bc3444ad6f
Juan Alberto <jarscode.dev@outlook.com>
2024-11-21 22:56:57 +0100

    colorscheme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,6 +18,7 @@ const App = () => {
       style={{ gridTemplateRows: "auto 1fr auto" }}
     >
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-100 dark:bg-neutral-900 text-black dark:text-white transition-all ease-in-out duration-150"></div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-100 text-black dark:text-neutral-100 dark:bg-neutral-950 dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] transition-all ease-in-out duration-150"></div>
       <Navbar />
       <Routes>
         {/* Public routes */}
